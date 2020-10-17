b9b868cf962b3331ce9a947754d9f57441fd95f2
yash1305 <yashmittal1305@gmail.com>
2020-10-18 03:09:09 +0530

    added code

---
+++ functions/firebase_config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBuBvTiN7ZZA2hO_GTYZ8c_h0mvToGBInw",
+  authDomain: "reactnative-707e8.firebaseapp.com",
+  databaseURL: "https://reactnative-707e8.firebaseio.com",
+  projectId: "reactnative-707e8",
+  storageBucket: "reactnative-707e8.appspot.com",
+  messagingSenderId: "38019890063",
+  appId: "1:38019890063:web:3609d8f608f8d37601a9fa",
+  measurementId: "G-F2RSE8NBWG"
+};;
+    
+exports.config = firebaseConfig;
